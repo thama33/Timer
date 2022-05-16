60f01b12409180960d6f33620d816e4abcd6643c
takuya.hamagami <thama0727@gmail.com>
2022-05-16 21:44:40 +0900

    ストーリーボード作成

---
+++ Timer/Timer2ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Timer2ViewController.swift
+//  Timer
+//
+//  Created by 濱上卓也 on 2022/05/16.
+//
+
+import UIKit
+
+class Timer2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
